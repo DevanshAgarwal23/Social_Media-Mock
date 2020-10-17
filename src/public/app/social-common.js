5cddbe41b1fb543ccf3e5a591f8bac0ecd83b0c7
Devansh Agarwal <devanshagarwal@pop-os.localdomain>
2020-10-17 17:54:07 +0530

    guest user login done

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -1,3 +1,25 @@
 $(()=>{
-    $('#navbar').load('../components/navbar.html')
+    $('#navbar').load('../components/navbar.html',loginIfNeeded)
+    $('#footer').load('../components/footer.html')
+
+    
 })
+
+
+
+function loginIfNeeded() {
+     window.currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null
+    if(!currentUser){
+        $.post('/api/users',{} , (user) => {
+            if(user){
+                console.log("registered current user", user.username)
+                window.localStorage.user = JSON.stringify(user)
+                currentUser = user
+                $('#nav-username').text(currentUser.username)
+            }
+        })
+    }else{
+        console.log('resuming session as ',currentUser.username)
+        $('#nav-username').text(currentUser.username)
+    }
+}
