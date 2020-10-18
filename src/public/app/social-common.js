58c7d494188900b525dae2dc3ec442b2e0f6f0a1
Devansh Agarwal <devanshagarwal@pop-os.localdomain>
2020-10-18 14:45:16 +0530

    get all posts and show on home

--- src/public/app/social-common.js
+++ src/public/app/social-common.js
@@ -1,5 +1,6 @@
 $(()=>{
     $('#navbar').load('../components/navbar.html',loginIfNeeded)
+    $('#content').load('../components/all-posts.html' , loadPosts)
     $('#footer').load('../components/footer.html')
 
     
@@ -12,14 +13,35 @@ function loginIfNeeded() {
     if(!currentUser){
         $.post('/api/users',{} , (user) => {
             if(user){
-                console.log("registered current user", user.username)
+                //console.log("registered current user", user.username)
                 window.localStorage.user = JSON.stringify(user)
                 currentUser = user
                 $('#nav-username').text(currentUser.username)
             }
         })
     }else{
-        console.log('resuming session as ',currentUser.username)
+        //console.log('resuming session as ',currentUser.username)
         $('#nav-username').text(currentUser.username)
     }
+}
+
+
+function loadPosts() {
+    $.get('/api/posts' , (posts) => {
+        for(let p of posts){
+           $('#posts-container').append(
+               $(`<div class="col-4">
+               <div class="card  m-2" >
+                <div class="card-body">
+                    <h5 class="card-title">${p.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+                    <p class="card-text">${p.body.substr(0,100)}<a href="#">...read more</a></p>
+                    <a href="#" class="card-link">Comment</a>
+                    <a href="#" class="card-link">Like</a>
+                </div>
+               </div>
+             </div>`)
+           )
+        }
+    })
 }
