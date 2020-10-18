3d437fef2f41f5fd20ced5eeac9bf59f75717bef
Devansh Agarwal <devanshagarwal@pop-os.localdomain>
2020-10-19 00:25:19 +0530

    adding new post done

--- src/public/app/write-post.js
+++ src/public/app/write-post.js
@@ -1,6 +1,23 @@
 
 
 
+$('#submit-post').click( () => {
+    let title = document.getElementById('title').value 
+    let body = document.getElementById('body').value
+    let userId = currentUser.id
+
+    $.post('/api/posts',{
+        userId: userId,
+        title:title,
+        body:body,
+        
+    })
+    
+})
+
+
+
+
 
 
 
